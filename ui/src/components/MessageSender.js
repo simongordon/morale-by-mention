34f9e9e6a3c75f79c912cd586022f4903bd9da1d
Simon Gordon <gordonsimon@hotmail.com>
2018-04-21 14:17:32 +1000

    Clean up code

--- ui/src/components/MessageSender.js
+++ ui/src/components/MessageSender.js
@@ -2,45 +2,39 @@ import React from 'react';
 import { Formik, Form, Field } from 'formik';
 import { postMessage } from '../modules/actions';
 
-class MessageSender extends React.Component {
-
-    render() {
-        return (
-            <Formik
-                initialValues={{
-                    message: ""
-                }}
-                validate={(values) => {
-                    const errors = {};
-                    if (!values.message) {
-                        errors.message = "Required";
+const MessageSender = (props) =>
+    <Formik
+        initialValues={{
+            message: ""
+        }}
+        validate={(values) => {
+            const errors = {};
+            if (!values.message) {
+                errors.message = "Required";
+            }
+            return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+            postMessage(props.to.id, values.message).then(() => {
+                setSubmitting(false)
+                props.after()
+            })
+        }}
+        render={({ errors, isSubmitting }) =>
+            <Form>
+                <div>
+                    <p>ID: {props.to.id}</p>
+                    <p>Name: {props.to.name}</p>
+                    <Field name="message" />
+                    {
+                        errors.message && <p>({errors.message})</p>
                     }
-                    return errors;
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                    postMessage(this.props.to.id, values.message).then(() => {
-                        setSubmitting(false)
-                        this.props.after()
-                    })
-                }}
-                render={({ errors, isSubmitting }) =>
-                    <Form>
-                        <div>
-                            <p>ID: {this.props.to.id}</p>
-                            <p>Name: {this.props.to.name}</p>
-                            <Field name="message" />
-                            {
-                                errors.message && <p>({errors.message})</p>
-                            }
-                        </div>
-                        <button type="submit" disabled={isSubmitting}>
-                            Submit
+                </div>
+                <button type="submit" disabled={isSubmitting}>
+                    Submit
                         </button>
-                    </Form>
-                }
-            />
-        )
-    }
-}
+            </Form>
+        }
+    />
 
 export default MessageSender;
--- ui/src/components/RandomUserGetter.js
+++ ui/src/components/RandomUserGetter.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form } from 'formik';
 import { getRandomUser } from '../modules/actions';
 
-class RandomUserGetter extends React.Component {
+const RandomUserGetter = (props) =>
+  <Formik
+    onSubmit={(_values, { setSubmitting }) => {
+      getRandomUser().then((result) => {
+        setSubmitting(false);
+        props.after(result);
+      })
+    }}
+    render={({ isSubmitting }) =>
+      <Form>
+        <div className="field">
+          {
+            isSubmitting && <p>Here's your spiner, Elliott!</p>
+          }
+          <button type="submit" className="ui fluid large teal submit button"
+            disabled={isSubmitting}>Spin</button>
+        </div>
+      </Form>
+    }
+  />
 
-  render() {
-    return <Formik
-      onSubmit={(_values, { setSubmitting }) => {
-        getRandomUser().then((result) => {
-          setSubmitting(false);
-          this.props.after(result);
-        })
-      }}
-      render={({isSubmitting}) => 
-         <Form>
-          <div className="field">
-            <button type="submit" className="ui fluid large teal submit button"
-              disabled={isSubmitting}>Spin</button>
-          </div>
-        </Form>
-      }
-    />
-  }
-}
 
 
 export default RandomUserGetter;
