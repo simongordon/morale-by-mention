f86c2a3081687e8ff7eb84591de5c53f147f72a1
elliott-cns <elliott@cnscloud.io>
2018-04-21 14:18:13 +1000

    serverless

--- api/serverless/mention/handler.js
+++ api/serverless/mention/handler.js
@@ -1,6 +1,4 @@
-'use strict';
-
-module.exports.hello = (event, context, callback) => {
+module.exports.mention = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
@@ -10,7 +8,4 @@ module.exports.hello = (event, context, callback) => {
   };
 
   callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
