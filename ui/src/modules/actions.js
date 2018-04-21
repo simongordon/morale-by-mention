29c0d88d10baadbca572c0cc7450209b46442d7d
elliott-cns <elliott@cnscloud.io>
2018-04-21 14:40:31 +1000

    stuff

--- ui/src/modules/actions.js
+++ ui/src/modules/actions.js
@@ -1,9 +1,14 @@
+import fetch from 'fetch';
+
+import { getRandom, postMention } from './externalUrls'; 
+
 export const getRandomUser = () => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve({id: 123, name: "John"})
-        }, 1000)
-    })
+    // return new Promise((resolve) => {
+    //     setTimeout(() => {
+    //         resolve({id: 123, name: "John"})
+    //     }, 1000)
+    // })
+    return fetch.fetchUrl(getRandom).then(user => console.log(user));
 }
 
 export const postMessage = (userId, message) => {
--- ui/src/modules/externalUrls.js
+++ ui/src/modules/externalUrls.js
@@ -1,2 +1,2 @@
-export const getRandom = "/ELLIOTT_PUT_SHIT_HERE_PLS";
-export const postMessage = "/ELLIOTT_PUT_SHIT_HERE_PLS";
+export const getRandom = 'http://52.237.251.57:3001/users/random';
+export const postMention = 'http://52.237.251.57:3001/mention';
