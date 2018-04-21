65563c51fbbfb774fd1394f71fc4a93370f04330
Harry Phillips <harry.phillips@cubic.com>
2018-04-21 17:26:32 +1000

    DNS entry for External URL

--- ui/src/modules/externalUrls.js
+++ ui/src/modules/externalUrls.js
@@ -1,2 +1,2 @@
-export const getRandom = 'http://52.237.251.57:3001/users/random';
-export const postMention = 'http://52.237.251.57:3001/mention';
+export const getRandom = 'http://mbm.australiaeast.cloudapp.azure.com:3001/users/random';
+export const postMention = 'http://mbm.australiaeast.cloudapp.azure.com:3001/mention';
