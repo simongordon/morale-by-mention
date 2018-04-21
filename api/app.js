7a63842fc9cd54b5f86dd7d3d7911f070c3256b0
elliott-cns <elliott@cnscloud.io>
2018-04-21 14:22:07 +1000

    updated api port to 3001, cra was using 3000

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
-const port = process.env.PORT = 3000;
+const port = process.env.PORT = 3001;
 
 app.use(routes);
 
