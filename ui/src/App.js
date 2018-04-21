af26b3dedabe4bf34f1ab234c7fd63e888846264
Simon Gordon <gordonsimon@hotmail.com>
2018-04-21 12:03:12 +1000

    Create search box

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'semantic-ui-css/components/site.css'
+import 'semantic-ui-css/components/header.css'
+import 'semantic-ui-css/components/grid.css'
+import 'semantic-ui-css/components/container.css'
+import 'semantic-ui-css/components/segment.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>
-          Yeet
-        </h1>
+      <div className="ui middle aligned center aligned grid">
+        <div className="column">
+          <h1 className="ui header">Employee search</h1>
+          <form className="ui large form">
+            <div className="ui stacked segment">
+              <div className="field">
+                <input type="text" placeholder="Name"/>
+              </div>
+              <div className="field">
+                <div className="ui fluid large teal submit button">Search</div>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
